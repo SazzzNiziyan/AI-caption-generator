027c2bee77e3a96931acde150f0157acc430f5f9
Sajal Singh <as3199432@gmail.com>
2025-10-13 21:32:41 +0530

    created	new file:   src/controllers/post.controller.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes')
+const postRoutes = require('./routes/post.routes')
 const cookieParser = require('cookie-parser')
 
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser)
 
-app.use('/auth',authRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/posts',postRoutes)
+
 
 module.exports = app;
