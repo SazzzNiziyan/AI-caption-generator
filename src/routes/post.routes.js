693db728457c7aa4a1460c85ca7143319858f516
Sajal Singh <as3199432@gmail.com>
2025-10-14 00:02:17 +0530

    modified:   src/controllers/post.controller.js

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware")
+const {createPostController} = require("../controllers/post.controller")
 const multer = reqiure("multer");
 
-const upload = multer({ storage: multer.memoryStorage()})
 
+const upload = multer({ storage: multer.memoryStorage()})
+  
 router.post('/', 
     authMiddleware,
     upload.single("image"),
