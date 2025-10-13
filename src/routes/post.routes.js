cb583194289220dda5555f63d65b25aaf10a5032
Sajal Singh <as3199432@gmail.com>
2025-10-13 21:31:13 +0530

    created new file:   src/routes/post.routes.js

---
+++ src/routes/post.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require("../middlewares/auth.middleware")
+const multer = reqiure("multer");
+
+const upload = multer({ storage: multer.memoryStorage()})
+
+router.post('/', 
+    authMiddleware,
+    upload.single("image"),
+    createPostController)
+
+module.exports = router;
