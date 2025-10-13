dada2722f4b8487e12a9cf8baa6ef5c0e68b545d
Sajal Singh <as3199432@gmail.com>
2025-10-14 00:20:00 +0530

    caption generation setup finished

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,7 +1,17 @@
 const postModel = require("../models/post.model");
+const generateCaption = require("../service/Ai.service");
 
 async function createPostController(req,res){
     const file = req.file; 
+    console.log("File received", file);
+
+    const base64Image = new Buffer.from(file.buffer).toString('base64');
+
+    const caption = await generateCaption(base64Image);
+
+    res.json({
+        capiton
+    })
 } 
 
 
